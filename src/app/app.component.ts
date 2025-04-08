bacf812cd11c1f509685c0d493d5d3014657c7e6
javaExactus <romulo.neves@exactus.com.br>
2025-04-08 14:00:26 -0300

    ajustes no codigo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { BannerComponent } from "./banner/banner.component";
+import { FormNovaTransacaoComponent } from "./form-nova-transacao/form-nova-transacao.component";
+import { Transacao } from './modelos/transacao';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [BannerComponent, FormNovaTransacaoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'anybank';
+  transacoes = signal<Transacao[]>([]);
+
+  processarTransacao(transacao : Transacao) {
+    this.transacoes.update((listaAtual) => [transacao,...listaAtual]);
+    console.log(this.transacoes());
+  }
 }
